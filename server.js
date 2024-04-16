220de6659732e0054c60f10323da2426cfe9ae19
princessige <161790190+princessige@users.noreply.github.com>
2024-04-16 14:28:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const port = 3000;
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.use(express.static('public'));
-
-
-app.post('/contact', (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-
-
-    console.log(`Received submission from: ${name}, ${email}, saying: ${message}`);
-    
-    
-    res.send('Received your message!');
+app.use(express.urlencoded({ extended: true }));
+www-form-urlencoded
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/contact.html');
 });
-
+app.post('/contact', (req, res) => {
+const name = req.body.name;
+const email = req.body.email
+const message = req.body.message
+    
+res.send(`Thanks for your message!`);
+});
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+console.log(`Server running on http://localhost:${port}`);
 });
